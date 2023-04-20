e422e7076c60dd506e82fe11d6531e82d817a9cb
Igor Muromov <igran2001@gmail.com>
2023-04-20 22:17:27 +0700

    Обновлен файл environment.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,6 @@
-export const environment = {};
+export const environment = {
+  production: false,
+  authApi: 'https://3b9f-37-21-203-5.ngrok-free.app/',
+  officeApi: 'https://localhost:7223/',
+  tokenWhiteListedDomains: ['localhost:7223']
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  authApi: 'https://localhost:7241/',
+  authApi: 'https://3b9f-37-21-203-5.ngrok-free.app/',
   officeApi: 'https://localhost:7223/',
   tokenWhiteListedDomains: ['localhost:7223']
 };
