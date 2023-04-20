f10d435554ee77194289b238a5dd6000b91fd677
Igor Muromov <igran2001@gmail.com>
2023-04-21 02:03:48 +0700

    Добавлено отображения кнопки выхода из приложения

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import {LoginService} from "../../services/login.service";
 
 @Component({
   selector: 'app-navigation',
@@ -8,7 +9,7 @@ import { Router } from '@angular/router';
 })
 export class NavigationComponent {
 
-  constructor(private router: Router) {
+  constructor(private router: Router, public loginService: LoginService) {
   }
 
   isAuth(): boolean {
