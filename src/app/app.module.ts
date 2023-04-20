6f6e9b8026b222a942d42c60acb9a414151a0643
Igor Muromov <igran2001@gmail.com>
2023-04-20 19:31:02 +0700

    Revert "Добавлена авторизация через jwt"
    This reverts commit 1f370a00325d1afbba8b99289b50287b40c912a9.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,18 +9,7 @@ import {UserComponent} from "./components/user.component";
 import { LegendDepartmentItemComponent } from './components/legend-department-item/legend-department-item.component';
 import { LegendComponent } from './components/legend/legend.component';
 import { MapEmployeeInfoComponent } from './components/map-employee-info/map-employee-info.component';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import { LoginPageComponent } from './pages/login-page/login-page.component';
-import { MainPageComponent } from './pages/main-page/main-page.component';
-import { NavigationComponent } from './components/navigation/navigation.component';
-import {AUTH_API_URL} from "./app-injection-tokens";
-import {environment} from "../environments/environment";
-import {JwtModule} from "@auth0/angular-jwt";
-import {ACCESS_TOKEN_KEY} from "./services/login.service";
-
-export function tokenGetter() {
-  return localStorage.getItem(ACCESS_TOKEN_KEY);
-}
+import {FormsModule} from "@angular/forms";
 
 // @ts-ignore
 @NgModule({
@@ -30,29 +19,15 @@ export function tokenGetter() {
     UserComponent,
     LegendDepartmentItemComponent,
     LegendComponent,
-    MapEmployeeInfoComponent,
-    LoginPageComponent,
-    MainPageComponent,
-    NavigationComponent
+    MapEmployeeInfoComponent
   ],
     imports: [
         BrowserModule,
         AppRoutingModule,
         HttpClientModule,
-        FormsModule,
-        ReactiveFormsModule,
-
-        JwtModule.forRoot({
-          config: {
-            tokenGetter,
-            allowedDomains: environment.tokenWhiteListedDomains
-          }
-        })
+        FormsModule
     ],
-  providers: [{
-    provide: AUTH_API_URL,
-    useValue: environment.authApi
-  }],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
