0f7887981fa80f5b34b2946a2bce0a2024897797
Igor Muromov <igran2001@gmail.com>
2023-04-20 20:31:19 +0700

    Добавлена пагинация

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,10 @@ import {UserComponent} from "./components/user.component";
 import { LegendDepartmentItemComponent } from './components/legend-department-item/legend-department-item.component';
 import { LegendComponent } from './components/legend/legend.component';
 import { MapEmployeeInfoComponent } from './components/map-employee-info/map-employee-info.component';
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
 
 @NgModule({
   declarations: [
@@ -18,13 +21,17 @@ import {FormsModule} from "@angular/forms";
     UserComponent,
     LegendDepartmentItemComponent,
     LegendComponent,
-    MapEmployeeInfoComponent
+    MapEmployeeInfoComponent,
+    NavigationComponent,
+    LoginPageComponent,
+    MainPageComponent
   ],
     imports: [
         BrowserModule,
         AppRoutingModule,
         HttpClientModule,
-        FormsModule
+        FormsModule,
+        ReactiveFormsModule
     ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/login-page/login-page.component.ts
@@ -0,0 +1,97 @@
+import { Component } from '@angular/core';
+import $ from "jquery";
+import {HttpClient} from "@angular/common/http";
+import {FormBuilder, Validators} from "@angular/forms";
+import {LoginService} from "../../services/login.service";
+import {Router} from "@angular/router";
+import {AppComponent} from "../../app.component";
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.scss']
+})
+export class LoginPageComponent {
+
+  loginForm = this.fb.group({
+    nameUser: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  });
+
+  constructor (private fb: FormBuilder, public loginService: LoginService, private router: Router, public appComponent: AppComponent) {
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+
+      this.loginService.login(this.loginForm.value)
+        .subscribe(
+          {
+            next: (response: any) => {
+              localStorage.setItem('token', response.access_token);
+              console.log(localStorage.getItem('token'));
+              this.router.navigateByUrl('');
+            },
+            error: (response: any) => {
+              if (response.status === 401) alert("Данные введены неверно");
+            }
+          }
+        );
+    }
+  }
+
+  /*onSubmitN() {
+    if (this.loginForm.valid) {
+
+      this.loginService.login(this.loginForm.value)
+        .subscribe(
+          {
+            next: (response: any) => {
+              localStorage.setItem('token', response.access_token);
+              console.log(localStorage.getItem('token'));
+              this.router.navigateByUrl('');
+            },
+            error: (response: any) => {
+              if (response.status === 401) alert("Данные введены неверно");
+            }
+          }
+        );
+    }
+  }*/
+
+  logIn() {
+
+    $('#popup-container').css('display', 'none');
+    $('#quit-btn').css('visibility', 'visible');
+  }
+
+  forgotPass() {
+    let logInPopup = document.getElementById('log-in-popup') as HTMLElement;
+    let forgotPassPopup = document.getElementById('recovery-popup') as HTMLElement;
+    logInPopup.style.display = 'none';
+    forgotPassPopup.style.display = 'inline';
+  }
+
+  backToLogIn() {
+    let logInPopup = document.getElementById('log-in-popup') as HTMLElement;
+    let forgotPassPopup = document.getElementById('recovery-popup') as HTMLElement;
+    logInPopup.style.display = 'inline';
+    forgotPassPopup.style.display = 'none';
+  }
+
+  sendOnEmail() {
+    let backBtn = document.getElementById('back-btn') as HTMLElement;
+    let recoveryTitle = document.getElementById('recovery-title') as HTMLElement;
+    let emailInput = document.getElementById('recover-email-input') as HTMLElement;
+    let sendOnEmailBtn = document.getElementById('send-on-email-btn') as HTMLElement;
+    backBtn.style.display = 'none';
+    recoveryTitle.innerText = 'На указанный адрес электронной почты была отправлена ссылка для сброса пароля';
+    recoveryTitle.style.maxWidth = '700px';
+    recoveryTitle.style.padding = '80px 50px'
+    emailInput.style.display = 'none';
+    sendOnEmailBtn.style.display = 'none';
+  }
+
+}
+
